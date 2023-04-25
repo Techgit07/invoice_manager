e1a3733e17339ce295df5638f5c7c2d0125fb9d5
Techgit07 <kaushalgithub@gmail.com>
2023-04-25 16:03:28 +0530

    changes: AWS credentials

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import server from './src';
 
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 80;
 server.listen(port, () => {
     console.log('\x1b[44m', `Server Is On URL http://localhost:${port}`, '\x1b[0m');
 })
