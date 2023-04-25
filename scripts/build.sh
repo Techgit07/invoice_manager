2f5661dc3b4551846b72ac8e94be862f3333e475
Techgit07 <kaushalgithub@gmail.com>
2023-04-25 17:02:21 +0530

    add: yml & docker pipeline

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-# If the directory, `dist`, doesn't exist, create `dist`
-stat build || mkdir build
-# Archive artifacts
-# cd build
-zip build/tap-digital-backend.zip -r build package.json config .platform .env .npmrc
