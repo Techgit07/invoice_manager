5d4d18ebcc36e71396464831c1fcfc5887dd7798
Techgit07 <kaushalgithub@gmail.com>
2023-04-26 09:48:13 +0530

    changes: server file

--- server.ts
+++ server.ts
@@ -2,5 +2,5 @@ import server from './src';
 
 const port = process.env.PORT || 80;
 server.listen(port, () => {
-    console.log('\x1b[44m', `Server Is On URL http://localhost:${port}`, '\x1b[0m');
+    console.log('\x1b[44m', `Server Is On URL http://localhost:${port}`, '\x1b[0m'); 
 })
